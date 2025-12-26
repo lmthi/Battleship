54989cddbdf3d82ddaa8712e01a5f92145f34d51
CAS_ual_TY <41904979+CAS-ual-TY@users.noreply.github.com>
2025-12-26 17:30:06 +0100

    Create convenient method to play against players

--- src/einfprog/bst/Main.java
+++ src/einfprog/bst/Main.java
@@ -1,31 +1,50 @@
 package einfprog.bst;
 
 import einfprog.bst.game.BoardType;
+import einfprog.bst.game.Coordinates;
 import einfprog.bst.game.ShipType;
-import einfprog.bst.player.HeatmapPlayer;
-import einfprog.bst.player.IPlayer;
-import einfprog.bst.player.ManualPlayer;
-import einfprog.bst.player.RandomPlayer;
+import einfprog.bst.player.*;
+import einfprog.bst.state.BoardMarker;
+import einfprog.bst.state.ShallowBoardView;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         Random random = new Random(100);
 
+        /*
+        testTourney(random);
+        /*/
+        testPlayer(random, new SearchAndSeekPlayer("search and seek player"));
+        //*/
+    }
+
+    private static void testPlayer(Random random, IPlayer player) {
+        List<IPlayer> players = new LinkedList<>();
+        players.add(new AutoPlacementManualPlayer());
+        players.add(player);
+
+        TournamentManager tm = new TournamentManager(BoardType.STANDARD_BOARD, ShipType.STANDARD_SHIPS, players, random);
+
+        try(ExecutorService executor =  Executors.newSingleThreadExecutor()) {
+            IPlayer winner = tm.freeForAll(players, 1);
+            System.out.println("WINNER: " + winner.getName());
+        }
+    }
+
+    private static void testTourney(Random random) {
         List<IPlayer> players = new LinkedList<>();
 
-        for(int i = 1; i <= 1; i++) {
+        for(int i = 1; i <= 3; i++) {
             players.add(new RandomPlayer("P".repeat(i) + i));
         }
-
         //players.add(new ManualPlayer());
 
         TournamentManager tm = new TournamentManager(BoardType.STANDARD_BOARD, ShipType.STANDARD_SHIPS, players, random);
